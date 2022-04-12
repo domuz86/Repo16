a0b73f87ca2074909de3f1fe40eb17279e5fc912
domuz86 <59613646+domuz86@users.noreply.github.com>
2022-04-12 15:31:52 +0200

    Update index.js

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,4 +1,32 @@
 // only change code below this line
+class Microsoft{
+    constructor(name){
+        this.name = name;
+    }
+    get name(){
+        return this._name;
+    }
+    set name(name){
+        this._name = name;        
+    }
+    occupation(){
+        return this.name+"is a philanthropist!"
+    }
+}
+class Facebook extends Microsoft{
+    constructor(name, age){
+        super(name);
+        this.age = age;
+    }
+        studentAge(){
+            return this.name + this.age;
+        }
+}
+let henry = new Microsoft("William Henry Gates III");
+console.log(henry.occupation());
+let elliot = new Facebook("Mark Elliot Zuckerberg ", 23);
+console.log(elliot.occupation());
+console.log(elliot.studentAge());
 // Only change code above this line
 module.exports = {
     Microsoft,
